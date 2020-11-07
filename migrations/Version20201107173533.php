31e48839a9f532e538286393eb1ede11c0fb43eb
Guillonaxel0714 <55197055+Guillonaxel0714@users.noreply.github.com>
2020-11-07 18:37:01 +0100

    fix bug relation manyToOne

---
+++ migrations/Version20201107173533.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201107173533 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE semaine (id INT AUTO_INCREMENT NOT NULL, module_id INT DEFAULT NULL, titre VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, INDEX IDX_7B4D8BEAAFC2B591 (module_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE semaine ADD CONSTRAINT FK_7B4D8BEAAFC2B591 FOREIGN KEY (module_id) REFERENCES module (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE semaine');
+    }
+}
--- src/Entity/Module.php
+++ src/Entity/Module.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ModuleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Module
      */
     private $nom;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Semaine::class, mappedBy="module")
+     */
+    private $semaines;
+
+    public function __construct()
+    {
+        $this->semaines = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,36 @@ class Module
     public function setNom(string $nom): self
     {
         $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Semaine[]
+     */
+    public function getSemaines(): Collection
+    {
+        return $this->semaines;
+    }
+
+    public function addSemaine(Semaine $semaine): self
+    {
+        if (!$this->semaines->contains($semaine)) {
+            $this->semaines[] = $semaine;
+            $semaine->setModule($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSemaine(Semaine $semaine): self
+    {
+        if ($this->semaines->removeElement($semaine)) {
+            // set the owning side to null (unless already changed)
+            if ($semaine->getModule() === $this) {
+                $semaine->setModule(null);
+            }
+        }
 
         return $this;
     }
